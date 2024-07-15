ff199128d14058ab080c988c0d58d49e0a875ee8
baejaewan <bjwan.career@gmail.com>
2024-07-16 00:19:17 +0900

    init

---
+++ src/main/java/com/bjcareer/stockservice/StockserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bjcareer.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjcareer/stockservice/StockserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjcareer.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
