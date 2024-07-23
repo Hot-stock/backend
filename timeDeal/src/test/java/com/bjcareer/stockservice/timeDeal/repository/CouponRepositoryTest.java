9e48422d91a0ebadfd24d4a930c13a2713a7c411
baejaewan <bjwan.career@gmail.com>
2024-07-23 17:51:20 +0900

    [feat] Solve Race Condition  - 사용자에게 쿠폰을 전달하면서 이벤트 테이블을 참조하게 되는데  다수의 사용자가 같은 참조 값을 읽어가면서 경쟁조건이 생기게 됨
     이를 비관적 락으로 해결함
    
     related to #8

---
+++ timeDeal/src/test/java/com/bjcareer/stockservice/timeDeal/repository/CouponRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.bjcareer.stockservice.timeDeal.repository;
+
+import com.bjcareer.stockservice.timeDeal.domain.Coupon;
+import com.bjcareer.stockservice.timeDeal.domain.TimeDealEvent;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class CouponRepositoryTest {
+    @Autowired EntityManager em;
+    @Autowired() CouponRepository couponRepository;
+
+    TimeDealEvent timeDealEvent;
+
+    @BeforeEach
+    void setUp() {
+        timeDealEvent = new TimeDealEvent(100);
+    }
+
+    @Test
+    void 쿠폰_저장하기() {
+        em.persist(timeDealEvent);
+
+        Coupon coupon = new Coupon(20.0, timeDealEvent);
+        String saveId = couponRepository.save(coupon);
+        Coupon findCoupon = em.find(Coupon.class, saveId);
+
+        assertEquals(findCoupon.getCouponNumber(), coupon.getCouponNumber());
+        assertEquals(findCoupon.getId(), saveId);
+    }
+
+    @Test
+    void 저장된_쿠폰_찾기() {
+        em.persist(timeDealEvent);
+
+        Coupon coupon = new Coupon(20.0, timeDealEvent);
+        em.persist(coupon);
+
+        Coupon coupon1 = couponRepository.findById(coupon.getId());
+
+        assertEquals(coupon1.getCouponNumber(), coupon.getCouponNumber());
+        assertEquals(coupon1.getId(), coupon.getId());
+    }
+}
