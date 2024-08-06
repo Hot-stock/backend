beea2ddd1be521c7d0cd96e3bf9298e9ea1c806e
baejaewan <bjwan.career@gmail.com>
2024-08-06 11:22:52 +0900

    [setting] Change setting file
    - 설정 파일들 변경

---
+++ userservice/src/main/java/com/bjcareer/userservice/AuthConfig.java
@@ -0,0 +1,25 @@
+package com.bjcareer.userservice;
+
+import com.bjcareer.userservice.domain.AuthTokenManager;
+import com.bjcareer.userservice.interceptor.LoginInterceptor;
+import com.bjcareer.userservice.repository.RedisRepository;
+import com.bjcareer.userservice.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthConfig implements WebMvcConfigurer {
+    private final JwtService jwtService;
+    private final AuthTokenManager authTokenManager;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor(jwtService))
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/api/v0/user/login", "/api/v0/user/logout", "/api/v0/auth/register");
+    }
+}
--- userservice/src/main/java/com/bjcareer/userservice/repository/MemoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bjcareer.userservice.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class MemoryRepository {
-}
