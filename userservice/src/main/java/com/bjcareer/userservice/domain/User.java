657a35674824ab64fc90ea885184776e175e8cab
baejaewan <bjwan.career@gmail.com>
2024-08-06 12:24:47 +0900

    [refactor] Add User domain
    - password 검증 로직 추가
    
    related to #17

--- userservice/src/main/java/com/bjcareer/userservice/domain/User.java
+++ userservice/src/main/java/com/bjcareer/userservice/domain/User.java
@@ -31,6 +31,13 @@ public class User {
         this.telegramId = telegramId;
     }
 
+    public boolean verifyPassword(String password){
+        if (this.password.equals(password)){
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public String toString() {
         return "User{" +
