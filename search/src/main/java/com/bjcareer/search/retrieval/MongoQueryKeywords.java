380bdaa717e5d897e19f52ff2fba1c8ea80eb277
baejaewan <bjwan.career@gmail.com>
2024-08-12 23:55:09 +0900

    [feat] Create Trie in Mongo
    - 트라이 자료구조를 이용해서 검색어 추천 기능을 구현함
    related to #31

---
+++ search/src/main/java/com/bjcareer/search/retrieval/MongoQueryKeywords.java
@@ -0,0 +1,9 @@
+package com.bjcareer.search.retrieval;
+
+public class MongoQueryKeywords {
+	public static final String KEYWORD = "keyword";
+	public static final String PARENT_ID = "parentId";
+	public static final String SEARCH_COUNT = "searchCount";
+	public static final String END_OF_WORD = "endOfWord";
+	public static final String KEY = "_id";
+}
