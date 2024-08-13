4ec7058a59dbb2a6b7c0088d095339d60a065cd1
baejaewan <bjwan.career@gmail.com>
2024-08-13 21:03:47 +0900

    [feat] Create Shard for noSQL
    - keyword를 이용해 샤딩을 구성함
    
    related to #34

--- search/src/main/java/com/bjcareer/search/MongoDBConfig.java
+++ search/src/main/java/com/bjcareer/search/MongoDBConfig.java
@@ -1,5 +1,8 @@
 package com.bjcareer.search;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,14 +14,18 @@ import com.mongodb.client.MongoDatabase;
 @Configuration
 public class MongoDBConfig {
 
-	@Value("${spring.data.mongodb.uri}")
-	private String uri;
+	@Value("${spring.data.mongodb.uris}")
+	private List<String> uris;
 
 	@Value("${spring.data.mongodb.database}")
 	private String database;
 
 	@Bean
-	public MongoDatabase mongoClient() {
-		return MongoClients.create(uri).getDatabase(database);
+	public List<MongoDatabase> mongoClient() {
+		List<MongoDatabase> databases = new ArrayList<>();
+		for (String uri : uris) {
+			databases.add(MongoClients.create(uri).getDatabase(database));
+		}
+		return databases;
 	}
 }
