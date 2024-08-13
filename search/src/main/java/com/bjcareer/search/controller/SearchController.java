c79324944b3bf2d2697c77b632fe88755de736a6
baejaewan <bjwan.career@gmail.com>
2024-08-13 10:57:59 +0900

    [feat] Add Controller update and query for recommand
    - 검색어 추천 및 업데이트를 위한 서비스 파일 업로드

--- search/src/main/java/com/bjcareer/search/controller/SearchController.java
+++ search/src/main/java/com/bjcareer/search/controller/SearchController.java
@@ -4,43 +4,40 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bjcareer.search.controller.dto.QueryRequestDTO;
-import com.bjcareer.search.controller.dto.QueryResponseDTO;
-import com.bjcareer.search.domain.entity.Suggestion;
-import com.bjcareer.search.service.SearchService;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import com.bjcareer.search.controller.dto.QueryRequestDTO;
+import com.bjcareer.search.controller.dto.QueryResponseDTO;
+import com.bjcareer.search.service.SearchService;
+
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-public class Search {
+public class SearchController {
 	private final SearchService searchService;
+
+	@PostMapping("/api/v0/search")
+	public ResponseEntity<?> search(@RequestBody QueryRequestDTO queryDTO) {
+		searchService.updateSearchCount(queryDTO.getKeyword());
+		return ResponseEntity.ok(null);
+	}
 
 	@GetMapping(("/api/v0/complete/search"))
 	public ResponseEntity<?> search(@RequestParam(name = "q", required = false) String query) {
 		if(query == null || query.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
+		List<String> suggestionKeyword = searchService.getSuggestionKeyword(query);
+		QueryResponseDTO queryResponseDTO = new QueryResponseDTO(suggestionKeyword, suggestionKeyword.size());
 
-		List<Suggestion> suggestionKeyword = searchService.getSuggestionKeyword(query);
-		log.debug("suggestionKeyword: {}", suggestionKeyword);
-
-		QueryResponseDTO queryResponseDTO = new QueryResponseDTO(suggestionKeyword);
 		return new ResponseEntity<>(queryResponseDTO, HttpStatus.OK);
-	}
-
-	@PostMapping("/api/v0/search")
-	public String search(@RequestBody QueryRequestDTO queryDTO) {
-		Suggestion suggestion = searchService.updateSearchCount(queryDTO.getKeyword());
-		return "ok";
 	}
 }
