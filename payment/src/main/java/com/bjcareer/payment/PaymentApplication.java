711be0018b61a42d3e60700a46bb977d2682feb1
baejaewan <bjwan.career@gmail.com>
2024-08-26 22:51:20 +0900

    [init] Project init

---
+++ payment/src/main/java/com/bjcareer/payment/PaymentApplication.java
@@ -0,0 +1,15 @@
+package com.bjcareer.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class PaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApplication.class, args);
+	}
+
+}
