2142b816de30f0ae9afb18f4385ce5933e87cca4
baejaewan <bjwan.career@gmail.com>
2024-09-03 21:07:02 +0900

    [feat] Add retry strategy to payment execution process
    - 네트워크 오류 또는 특정 PSP 오류 응답에 따라 재시도 전략을 추가했습니다.
    
    related to #55

--- payment/src/main/java/com/bjcareer/payment/adapter/out/web/toss/executor/TossPaymentExecutor.java
+++ payment/src/main/java/com/bjcareer/payment/adapter/out/web/toss/executor/TossPaymentExecutor.java
@@ -1,8 +1,15 @@
 package com.bjcareer.payment.adapter.out.web.toss.executor;
 
+import java.time.Duration;
+import java.time.temporal.TemporalUnit;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.bjcareer.payment.adapter.out.web.toss.exception.PspConfirmationException;
+import com.bjcareer.payment.adapter.out.web.toss.exception.TossErrorCode;
+import com.bjcareer.payment.adapter.out.web.toss.response.TossFailureResponse;
 import com.bjcareer.payment.adapter.out.web.toss.response.TossPaymentExecutionResponse;
 import com.bjcareer.payment.application.domain.PaymentConfirmResult;
 import com.bjcareer.payment.application.domain.PaymentExecutionResult;
@@ -10,6 +17,7 @@ import com.bjcareer.payment.application.domain.PaymentExecutionResult;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 @Component
 @RequiredArgsConstructor
@@ -27,10 +35,18 @@ public class TossPaymentExecutor implements PaymentExecutor {
 			.retrieve()
 			.onStatus(
 				httpStatusCode -> httpStatusCode.is4xxClientError() || httpStatusCode.is5xxServerError(),
-				clientResponse -> Mono.error(new RuntimeException("Failed to communicate with Toss: " + clientResponse.statusCode()))
+				clientResponse -> clientResponse.bodyToMono(TossFailureResponse.class)
+					.flatMap(it -> {
+						TossErrorCode tossErrorCode = TossErrorCode.valueOf(it.getCode());
+						PspConfirmationException pspConfirmationException = new PspConfirmationException(
+							it.getCode(), it.getMessage(), tossErrorCode);
+						return Mono.error(pspConfirmationException);
+					})
 			)
-			.bodyToMono(TossPaymentExecutionResponse.class);
+			.bodyToMono(TossPaymentExecutionResponse.class)
+			.retryWhen(Retry.backoff(3, Duration.ofSeconds(2)).jitter(2.0));
 
 		return tossPaymentExecutionResponseMono.flatMap(it -> Mono.just(new PaymentExecutionResult(it.getPaymentKey(), it.getOrderId(), it.getOrderName(), it.getTotalAmount(), it.getStatus(), it.getRequestedAt(), it.getApprovedAt(), true, false, false)));
 	}
+
 }
