76d2304b0f6fa4ddf4d772c2a91a85cb4e1d5ec5
baejaewan <bjwan.career@gmail.com>
2024-09-18 00:29:52 +0900

    [feat] Add custom thema - 사용자가 테마 추가 가능

--- search/src/main/java/com/bjcareer/search/controller/StockController.java
+++ search/src/main/java/com/bjcareer/search/controller/StockController.java
@@ -1,17 +1,25 @@
 package com.bjcareer.search.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bjcareer.search.controller.dto.StockAdditionRequestDTO;
 import com.bjcareer.search.controller.dto.SearchStockResponseDTO;
+import com.bjcareer.search.controller.dto.StockAdditionResponseDTO;
 import com.bjcareer.search.domain.entity.Thema;
 import com.bjcareer.search.service.StockService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,10 +31,20 @@ public class StockController {
 	private final StockService stockService;
 
 	@GetMapping("")
-	public Response<?> getStockOfThema(@RequestParam(name = "q") String stock) {
-		log.info("q: {}", stock);
+	public ResponseEntity<?> getStockOfThema(@RequestParam(name = "q") String stock) {
+		log.debug("q: {}", stock);
 		List<Thema> stockOfThema = stockService.getStockOfThema(stock);
 		SearchStockResponseDTO searchStockResponseDTO = new SearchStockResponseDTO(stockOfThema);
-		return new Response<>(HttpStatus.OK, "OK", searchStockResponseDTO.responses);
+		return ResponseEntity.status(HttpStatus.OK).body(searchStockResponseDTO.responses);
+	}
+
+	@PostMapping
+	public ResponseEntity<?> addStockOfThema(@RequestBody @Valid StockAdditionRequestDTO requestDTO) {
+		log.debug("request: {}", requestDTO);
+
+		Thema thema = stockService.addStockThema(requestDTO.getCode(), requestDTO.getStockName(), requestDTO.getThema());
+		StockAdditionResponseDTO responseDTO = new StockAdditionResponseDTO(thema.getId(),
+			thema.getStock().getName(), thema.getThemaInfo().getName(), thema.getStock().getCode());
+		return ResponseEntity.status(HttpStatus.CREATED).body(new Response<>(HttpStatus.CREATED, "CREATED", responseDTO));
 	}
 }
---
+++ search/src/main/java/com/bjcareer/search/service/exceptions/StockServiceException.java
@@ -0,0 +1,7 @@
+package com.bjcareer.search.service.exceptions;
+
+public class StockServiceException extends RuntimeException {
+	public StockServiceException(String message) {
+		super(message);
+	}
+}
