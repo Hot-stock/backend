5c4e769936b83866c1f3749e122066896b09bdb5
baejaewan <bjwan.career@gmail.com>
2024-09-18 00:42:06 +0900

    [feat] Add req/res dto - Response dto 추가

--- search/src/main/java/com/bjcareer/search/repository/stock/ThemaInfoRepository.java
+++ search/src/main/java/com/bjcareer/search/repository/stock/ThemaInfoRepository.java
@@ -1,6 +1,7 @@
 package com.bjcareer.search.repository.stock;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,8 @@ public interface ThemaInfoRepository extends JpaRepository<ThemaInfo, Long> {
 
 	@Query("SELECT t FROM ThemaInfo t join fetch t.themas s join fetch s.stock  WHERE t.name LIKE %:thema%")
 	List<ThemaInfo> findByNameContains(String thema);
+
+
+	@Query("SELECT t FROM ThemaInfo t WHERE t.name = :thema")
+	Optional<ThemaInfo> findByThemaName(String thema);
 }
--- search/src/main/java/com/bjcareer/search/repository/stock/ThemaRepository.java
+++ search/src/main/java/com/bjcareer/search/repository/stock/ThemaRepository.java
@@ -1,6 +1,7 @@
 package com.bjcareer.search.repository.stock;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,10 +9,9 @@ import org.springframework.data.jpa.repository.Query;
 import com.bjcareer.search.domain.entity.Thema;
 
 public interface ThemaRepository extends JpaRepository<Thema, Long> {
-
-	@Query("SELECT t FROM Thema t WHERE t.themaInfo.name LIKE %:themaInfo%")
-	List<Thema> findByThemaInfo(String themaInfo);
-
 	@Query("SELECT t FROM Thema t join fetch t.stock s join fetch  t.themaInfo ti WHERE t.stock.name LIKE %:name%")
-	List<Thema> findByStockName(String name);
+	List<Thema> findByStockNameContaining(String name);
+
+	@Query("SELECT t FROM Thema t WHERE t.stock.name = :stockName AND t.themaInfo.name = :themaName")
+	Optional<Thema> findByStockNameAndThemaName(String stockName, String themaName);
 }
