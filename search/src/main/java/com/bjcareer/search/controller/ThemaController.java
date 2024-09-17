24ebb1722b4ffc838fa8dd48603e77d41a05bdc6
baejaewan <bjwan.career@gmail.com>
2024-09-18 01:23:27 +0900

    [feat] Add Thema Search Service
    - 테마 이름으로 주식 검색 서비스

---
+++ search/src/main/java/com/bjcareer/search/controller/ThemaController.java
@@ -0,0 +1,32 @@
+package com.bjcareer.search.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bjcareer.search.controller.dto.SearchThemaResponseDTO;
+import com.bjcareer.search.domain.entity.ThemaInfo;
+import com.bjcareer.search.service.ThemaService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v0/thema")
+public class ThemaController {
+	private final ThemaService themaService;
+
+	@GetMapping("")
+	public Response<?> searchThema(@RequestParam(name = "q") String thema, @RequestParam(required = false) Long marketAmount) {
+		if (thema.isEmpty()) {
+			return new Response<>(HttpStatus.BAD_REQUEST, "thema is empty", null);
+		}
+		List<ThemaInfo> themaInfos = themaService.searchThema(thema);
+		SearchThemaResponseDTO searchThemaResponseDTO = new SearchThemaResponseDTO(themaInfos);
+		return new Response<>(HttpStatus.OK, "OK", searchThemaResponseDTO.response);
+	}
+}
---
+++ search/src/test/java/com/bjcareer/search/service/ThemaServiceTest.java
@@ -0,0 +1,21 @@
+package com.bjcareer.search.service;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.bjcareer.search.domain.entity.ThemaInfo;
+
+@SpringBootTest
+class ThemaServiceTest {
+	@Autowired ThemaService service;
+
+	@Test
+	void test_search() {
+		List<ThemaInfo> themaInfos = service.searchThema("출산");
+		System.out.println("themaInfos = " + themaInfos);
+	}
+
+}
