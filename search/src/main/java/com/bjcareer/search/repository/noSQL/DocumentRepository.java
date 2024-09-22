d8898a33e36b15297423823ec66fd454732b758b
baejaewan <bjwan.career@gmail.com>
2024-09-22 17:12:20 +0900

    [refactor] Update insertAndUpdateKeyword 도큐먼트 파일 레포 업데이트

--- search/src/main/java/com/bjcareer/search/repository/noSQL/DocumentRepository.java
+++ search/src/main/java/com/bjcareer/search/repository/noSQL/DocumentRepository.java
@@ -7,15 +7,18 @@ import java.util.List;
 
 import org.bson.Document;
 import org.bson.types.ObjectId;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.stereotype.Repository;
 
-import com.bjcareer.search.retrieval.noSQL.DocumentQueryKeywords;
+import com.bjcareer.search.candidate.noSQL.DocumentQueryKeywords;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Updates;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Repository
+@Slf4j
 public class DocumentRepository {
 	private final MongoCollection<Document> collection;
 	public static final String TRIE_COLLECTION_NAME = "Trie";
@@ -24,39 +27,88 @@ public class DocumentRepository {
 		this.collection = database.getCollection(TRIE_COLLECTION_NAME);
 	}
 
-	public Document findSingleByKeyword(String keyword, String query){
-		return collection.find(eq(keyword, query)).first();
+	public void insertAndUpdateKeyword(String keyword) {
+		StringBuilder query = new StringBuilder();
+		ObjectId parendId = null;
+		Document lastDocument = null;
+
+		for (int i = 0; i < keyword.length(); i++) {
+			query.append(keyword.charAt(i));
+
+			Document parentNode = findSingleByKeyword(query.toString());
+
+			if (parentNode != null) {
+				parendId = parentNode.getObjectId(DocumentQueryKeywords.KEY);
+			} else {
+				lastDocument = changeNodeToDocument(query.toString(), parendId);
+				parendId = saveDocument(lastDocument);
+			}
+		}
+
+		if (lastDocument == null) {
+			return;
+		}
+
+		updateSearchCount(keyword);
+		setEndOfWord(parendId, true);
+		updateChildToParent(lastDocument, parendId);
 	}
 
-	public MongoCursor<Document> findAllByKeyword(String keyword, Object query){
-		return collection.find(eq(keyword, query)).cursor();
+	public void updateChildToParent(Document endOfDocument, ObjectId childId) {
+		ObjectId parentId = endOfDocument.getObjectId(DocumentQueryKeywords.PARENT_ID);
+
+		while (parentId != null) {
+			setChildIdToParentDocument(childId, parentId);
+			parentId = findByObjectId(parentId).getObjectId(DocumentQueryKeywords.PARENT_ID);
+		}
 	}
 
-	public Document findByObjectId(String keyword, ObjectId objectId){
+	private Document changeNodeToDocument(String query, ObjectId parendId) {
+		Document document = new Document();
+
+		document.put(DocumentQueryKeywords.KEYWORD, query);
+		document.put(DocumentQueryKeywords.PARENT_ID, parendId);
+		document.put(DocumentQueryKeywords.SEARCH_COUNT, 0);
+		document.put(DocumentQueryKeywords.END_OF_WORD, false);
+
+		return document;
+	}
+
+	public Document findSingleByKeyword(String query) {
+		return collection.find(eq(DocumentQueryKeywords.KEYWORD, query)).first();
+	}
+
+	public Document findByObjectId(ObjectId objectId) {
 		return collection.find(eq(DocumentQueryKeywords.KEY, objectId)).first();
 	}
 
-	public ObjectId saveDocument(String keyword, Document document){
+	public ObjectId saveDocument(Document document) {
 		collection.insertOne(document);
 		return document.getObjectId(DocumentQueryKeywords.KEY);
 	}
 
-	public void setChildIdToParentDocument(String keyword, ObjectId childId, ObjectId parentId){
+	public void setChildIdToParentDocument(ObjectId childId, ObjectId parentId) {
 		collection.updateOne(
 			eq(DocumentQueryKeywords.KEY, parentId),
 			Updates.addToSet("childs", childId)
 		);
 	}
 
-	public void updateKeyword(String keyword, String key, ObjectId id, Object object){
-		collection.updateOne(eq(DocumentQueryKeywords.KEY, id), Updates.set(key, object));
+	public void setEndOfWord(ObjectId id, Boolean entOfWord) {
+		collection.updateOne(eq(DocumentQueryKeywords.KEY, id),
+			Updates.set(DocumentQueryKeywords.END_OF_WORD, entOfWord));
 	}
 
 	public void updateSearchCount(String keyword){
-		collection.updateOne(eq(DocumentQueryKeywords.KEYWORD, keyword), Updates.inc(DocumentQueryKeywords.KEYWORD, 1));
+		if (findSingleByKeyword(keyword) == null) {
+			log.error("keyword not found");
+			throw new InvalidDataAccessApiUsageException("keyword not found");
+		}
+		collection.updateOne(eq(DocumentQueryKeywords.KEYWORD, keyword),
+			Updates.inc(DocumentQueryKeywords.SEARCH_COUNT, 1));
 	}
 
-	public List<String> getkeyworkList(String keyword, Document rootDocument){
+	public List<String> getkeyworkList(Document rootDocument) {
 		List<String> result = new ArrayList<>();
 
 		if (rootDocument == null) {
@@ -67,9 +119,13 @@ public class DocumentRepository {
 
 		if (childs != null) {
 			childs.forEach(
-				c -> result.add(findByObjectId(keyword, c).get(DocumentQueryKeywords.KEYWORD, String.class)));
+				c -> result.add(findByObjectId(c).get(DocumentQueryKeywords.KEYWORD, String.class)));
 		}
 
 		return result;
+	}
+
+	public void deleteOne(ObjectId objectId) {
+		collection.deleteOne(eq(DocumentQueryKeywords.KEY, objectId));
 	}
 }
