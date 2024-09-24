6c8210b95483f432295d4de14b64a0f1161cdc73
baejaewan <bjwan.career@gmail.com>
2024-09-24 15:14:53 +0900

    [feat] Fetch Keyword count usng naver api  - 네이버 api를 이용해서 검색 횟수 fetch

---
+++ search/src/main/java/com/bjcareer/search/NaverApiAdConfig.java
@@ -0,0 +1,23 @@
+package com.bjcareer.search;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import com.bjcareer.search.out.api.ApiAdkeyword;
+
+@Configuration
+public class NaverApiAdConfig {
+	@Value("${naver.keyword-custom-id}")
+	private String CUSTOMER_ID;
+	@Value("${naver.keyword-access-key}")
+	private String ACCESS_KEY;
+	@Value("${naver.keyword-secret-key}")
+	private String SECRET_KEY;
+
+	@Bean
+	public ApiAdkeyword apiAdkeyword(RestTemplate restTemplate) {
+		return new ApiAdkeyword(CUSTOMER_ID, ACCESS_KEY, SECRET_KEY, restTemplate);
+	}
+}
---
+++ search/src/main/java/com/bjcareer/search/out/api/utils/Signatures.java
@@ -0,0 +1,32 @@
+package com.bjcareer.search.out.api.utils;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+import java.security.SignatureException;
+import java.util.Base64;
+
+public class Signatures {
+	private static final String HMAC_SHA256 = "HmacSHA256";  // Java 표준 알고리즘 이름
+
+	public static String of(String timestamp, String method, String resource, String key) throws SignatureException {
+		return of(timestamp + "." + method + "." + resource, key);
+	}
+
+	public static String of(String data, String key) throws SignatureException {
+		try {
+			Mac mac = Mac.getInstance(HMAC_SHA256);  // 기본 프로바이더 사용
+
+			mac.init(new SecretKeySpec(key.getBytes("utf-8"), HMAC_SHA256));
+			byte[] rawHmac = mac.doFinal(data.getBytes());
+
+			return Base64.getEncoder().encodeToString(rawHmac);
+		} catch (GeneralSecurityException e) {
+			throw new SignatureException("Failed to generate signature.", e);
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
