48d2a5610500197e89664491e92ba79a8ceade61
baejaewan <bjwan.career@gmail.com>
2024-09-30 00:47:03 +0900

    [refactor] Add CookieHelper
    - 쿠키 핼퍼를 추가함으로써 SRP원칙을 달성하도록 함
    
    related to #3

--- userservice/src/main/java/com/bjcareer/userservice/controller/UserController.java
+++ userservice/src/main/java/com/bjcareer/userservice/controller/UserController.java
@@ -11,13 +11,13 @@ import com.bjcareer.userservice.application.auth.ports.LoginCommand;
 import com.bjcareer.userservice.application.auth.ports.LoginUsecase;
 import com.bjcareer.userservice.application.token.ports.TokenUsecase;
 import com.bjcareer.userservice.application.token.valueObject.JwtTokenVO;
+import com.bjcareer.userservice.common.CookieHelper;
 import com.bjcareer.userservice.controller.dto.UserDto.LoginRequest;
 import com.bjcareer.userservice.controller.dto.UserDto.LoginResponse;
 import com.bjcareer.userservice.domain.entity.RoleType;
 import com.bjcareer.userservice.domain.entity.User;
 import com.bjcareer.userservice.security.HasRole;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -37,8 +37,9 @@ public class UserController {
         User user = loginUsecase.login(command);
         JwtTokenVO jwtTokenVO = tokenUsecase.generateToken(user);
 
-        setCookieForRefreshToken(response, jwtTokenVO);
-        setCookieForSessionId(response, jwtTokenVO);
+        CookieHelper.setCookieForRefreshToken(response, jwtTokenVO);
+        CookieHelper.setCookieForSessionId(response, jwtTokenVO);
+
 
         LoginResponse res = new LoginResponse(jwtTokenVO.getAccessToken());
 
@@ -52,26 +53,5 @@ public class UserController {
         JwtTokenVO jwtTokenVO = tokenUsecase.generateAccessTokenViaRefresh(sessionId, refreshToken);
         LoginResponse res = new LoginResponse(jwtTokenVO.getAccessToken());
         return ResponseEntity.ok(res);
-    }
-
-    private void setCookieForRefreshToken(HttpServletResponse response, JwtTokenVO jwtTokenVO) {
-        Cookie cookie = new Cookie("refreshToken", jwtTokenVO.getRefreshToken());
-
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-
-        response.addCookie(cookie);
-    }
-
-
-    private void setCookieForSessionId(HttpServletResponse response, JwtTokenVO jwtTokenVO) {
-        Cookie cookie = new Cookie("sessionId", jwtTokenVO.getSessionId());
-
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-
-        response.addCookie(cookie);
     }
 }
