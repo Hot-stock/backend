96d7769c6c825dd482152936445d87406a37013b
baejaewan <bjwan.career@gmail.com>
2024-10-04 19:40:48 +0900

    [hotfix] Rename constance name #userservice

--- userservice/src/main/java/com/bjcareer/userservice/common/CookieHelper.java
+++ userservice/src/main/java/com/bjcareer/userservice/common/CookieHelper.java
@@ -8,18 +8,18 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class CookieHelper {
 
-	public static final String REFRESH_TOKEN = "refreshToken";
-	public static final String ACCESS_TOKEN = "accessToken";
-	public static final String SESSION_ID = "sessionId";
+	public static final String REFRESH_TOKEN = "REFRESH_TOKEN";
+	public static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+	public static final String SESSION_ID = "SESSION_ID";
 
 	private static final String COOKIE_PATH = "/";
 
 	// 쿠키 설정 메서드
 	public static void setCookieAuthClient(HttpServletResponse response, JwtTokenVO jwtTokenVO) {
-		addCookie(response, ACCESS_TOKEN, jwtTokenVO.getAccessToken(), true, JWTUtil.ACCESS_TOKEN_EXPIRE_DURATION_SEC);
-		addCookie(response, REFRESH_TOKEN, jwtTokenVO.getRefreshToken(), true,
+		addCookie(response, ACCESS_TOKEN, jwtTokenVO.getAccessToken(), JWTUtil.ACCESS_TOKEN_EXPIRE_DURATION_SEC);
+		addCookie(response, REFRESH_TOKEN, jwtTokenVO.getRefreshToken(),
 			JWTUtil.REFRESH_TOKEN_EXPIRE_DURATION_SEC);
-		addCookie(response, SESSION_ID, jwtTokenVO.getSessionId(), true, JWTUtil.REFRESH_TOKEN_EXPIRE_DURATION_SEC);
+		addCookie(response, SESSION_ID, jwtTokenVO.getSessionId(), JWTUtil.REFRESH_TOKEN_EXPIRE_DURATION_SEC);
 	}
 
 	// 쿠키 제거 메서드
@@ -30,11 +30,11 @@ public class CookieHelper {
 	}
 
 	// 쿠키 추가 헬퍼 메서드
-	private static void addCookie(HttpServletResponse response, String name, String value, boolean httpOnly,
+	private static void addCookie(HttpServletResponse response, String name, String value,
 		int maxAge) {
 		Cookie cookie = new Cookie(name, value);
 		cookie.setPath(COOKIE_PATH);
-		cookie.setHttpOnly(httpOnly);
+		cookie.setHttpOnly(true);
 		cookie.setSecure(true);
 		cookie.setMaxAge(maxAge);
 		response.addCookie(cookie);
--- userservice/src/main/java/com/bjcareer/userservice/interceptor/LoginInterceptor.java
+++ userservice/src/main/java/com/bjcareer/userservice/interceptor/LoginInterceptor.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.bjcareer.userservice.application.ports.in.TokenUsecase;
+import com.bjcareer.userservice.common.CookieHelper;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,9 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class LoginInterceptor implements HandlerInterceptor {
-    public static final String ACCESS_TOKEN = "accessToken";
-    public static final String SESSION_ID = "sessionId";
-
     private final TokenUsecase tokenUsecase;
 
     @Override
@@ -30,17 +28,21 @@ public class LoginInterceptor implements HandlerInterceptor {
             return false;
         }
 
-        Optional<Cookie> accessToken = Arrays.stream(cookies).filter(c -> c.getName().equals(ACCESS_TOKEN)).findFirst();
-        Optional<Cookie> sessionId = Arrays.stream(cookies).filter(c -> c.getName().equals(SESSION_ID)).findFirst();
+        Optional<Cookie> accessToken = Arrays.stream(cookies)
+            .filter(c -> c.getName().equals(CookieHelper.ACCESS_TOKEN))
+            .findFirst();
+        Optional<Cookie> sessionId = Arrays.stream(cookies)
+            .filter(c -> c.getName().equals(CookieHelper.SESSION_ID))
+            .findFirst();
 
-        request.setAttribute(SESSION_ID, sessionId.get().getValue());
-
-        if (accessToken.isEmpty()) {
+        if (sessionId.isEmpty()) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return false;
         }
 
-        if (sessionId.isEmpty()) {
+        request.setAttribute(CookieHelper.SESSION_ID, sessionId.get().getValue());
+
+        if (accessToken.isEmpty()) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return false;
         }
