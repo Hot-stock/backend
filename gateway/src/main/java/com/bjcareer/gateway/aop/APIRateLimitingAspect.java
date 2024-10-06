b7ac2f68e4354c7d79de3f2d01a275e6d84c0522
baejaewan <bjwan.career@gmail.com>
2024-10-06 21:33:08 +0900

    [feat] Add Api request limit  - api 요청 수를 제어하는 기능 구현
     related to #21

--- gateway/src/main/java/com/bjcareer/gateway/aop/APIRateLimitingAspect.java
+++ gateway/src/main/java/com/bjcareer/gateway/aop/APIRateLimitingAspect.java
@@ -4,8 +4,11 @@ import java.time.Clock;
 import java.util.Arrays;
 import java.util.Optional;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import com.bjcareer.gateway.aop.ports.out.RateLimitPort;
@@ -23,13 +26,34 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class RateLimitingAspect {
+public class APIRateLimitingAspect {
 	private final JWTUtil jwtUtil;
 	private final RateLimitPort rateLimitPort;
 	private final Clock clock;
 
-	@Before("execution(* com.bjcareer.gateway.application..*(..)) && args(request,..)")
-	public void rateLimiting(HttpServletRequest request) {
+	@Pointcut("@annotation(com.bjcareer.gateway.aop.APIRateLimit)")
+	private void cut() {
+	}
+
+	@Before("cut()")
+	public void rateLimiting(JoinPoint joinPoint) {
+		Signature signature = joinPoint.getSignature();
+		log.debug("Rate Limiting Aspect {}", signature.getName());
+
+		//매개변수들 읽음
+		Object[] args = joinPoint.getArgs();
+		HttpServletRequest request = null;
+		for (Object arg : args) {
+			if (arg instanceof HttpServletRequest) {
+				request = (HttpServletRequest)arg;
+			}
+		}
+
+		if (request == null) {
+			log.debug("Request is Null");
+			return;
+		}
+
 		// 여러 메서드 시그니처에서도 HttpServletRequest만 있으면 실행됨
 		log.debug("Rate Limiting: " + request.getRemoteAddr());
 		Optional<Cookie> accessToken = Arrays.stream(request.getCookies())
@@ -52,8 +76,7 @@ public class RateLimitingAspect {
 		if (tokenBucket.isEmpty()) {
 			log.debug("Token Bucket is Empty");
 			rateLimitPort.saveTokenBucket(key, new TokenBucket(clock));
-		}
-		else{
+		} else {
 			log.debug("Token Bucket is Not Empty");
 			TokenBucket apiTokenBuket = tokenBucket.get();
 			log.debug("Starting API LIMIT : {}", apiTokenBuket.getAvailableTokens());
