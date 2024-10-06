faf7a1182d001b842176b5f5d86eae83f5afc218
baejaewan <bjwan.career@gmail.com>
2024-10-06 14:54:27 +0900

    [refactor] Add config file  - config파일 추가 #gateway

---
+++ gateway/src/main/java/com/bjcareer/gateway/SearchWebClientConfig.java
@@ -0,0 +1,21 @@
+package com.bjcareer.gateway;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class SearchWebClientConfig {
+	@Value("${search.url}")
+	private String baseURL;
+
+	@Bean
+	@Qualifier("searchWebClient")
+	public WebClient webClient(WebClient.Builder builder) {
+		builder.baseUrl(baseURL)
+			.defaultHeader("Content-Type", "application/json");
+		return builder.build();
+	}
+}
