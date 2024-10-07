56e73f4cc57f1284a912099e77b83d76289618bf
baejaewan <bjwan.career@gmail.com>
2024-10-07 20:02:48 +0900

    [refactor] Remove unused interface #gateway

--- gateway/src/test/java/com/bjcareer/gateway/out/api/AuthServerAPIAdapterTest.java
+++ gateway/src/test/java/com/bjcareer/gateway/out/api/AuthServerAPIAdapterTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.bjcareer.gateway.application.ports.in.LoginCommand;
 import com.bjcareer.gateway.application.ports.out.LoginCommandPort;
 import com.bjcareer.gateway.domain.JWTDomain;
 
@@ -12,7 +13,7 @@ class AuthServerAPIAdapterTest {
 	@Test
 	void login() {
 		// Given
-		LoginCommandPort loginCommand = new LoginCommandPort("wodhksqw@naver.com", "friend77asd@");
+		LoginCommand loginCommand = new LoginCommand("wodhksqw@naver.com", "friend77asd");
 		WebClient webClient = createWebClient("http://3.34.191.223:8080");
 		AuthServerAPIAdapter authServerAPIAdapter = new AuthServerAPIAdapter(webClient);
 		JWTDomain res = authServerAPIAdapter.login(loginCommand);
