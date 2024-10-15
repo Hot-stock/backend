34e4afa881e33dc91bf7d5761f00585150b174c5
baejaewan <bjwan.career@gmail.com>
2024-10-15 13:01:33 +0900

    [feat] Add Logger Using Request Bean scope
    related to #33

---
+++ gateway/src/main/java/com/bjcareer/gateway/common/Logger.java
@@ -0,0 +1,53 @@
+package com.bjcareer.gateway.common;
+
+import static org.springframework.context.annotation.ScopedProxyMode.*;
+import static org.springframework.web.context.WebApplicationContext.*;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Scope(value = SCOPE_REQUEST, proxyMode = TARGET_CLASS)
+@Component
+@Slf4j
+public class Logger {
+	private String prefix;
+	@Setter
+	private String url;
+
+	public void debug(String format, Object... arguments) {
+		if (log.isDebugEnabled()) {
+			log.debug(getPrefix() + format, arguments);
+		}
+	}
+
+	public void info(String format, Object... arguments) {
+		if (log.isInfoEnabled()) {
+			log.info(getPrefix() + format, arguments);
+		}
+	}
+
+	public void warn(String format, Object... arguments) {
+		if (log.isWarnEnabled()) {
+			log.warn(getPrefix() + format, arguments);
+		}
+	}
+
+	public void error(String format, Object... arguments) {
+		if (log.isErrorEnabled()) {
+			log.error(getPrefix() + format, arguments);
+		}
+	}
+
+	@PostConstruct
+	public void init() {
+		this.prefix = "[" + java.util.UUID.randomUUID().toString().substring(0, 8) + "]";
+	}
+
+	private String getPrefix() {
+		return this.prefix + " " + this.url + " ";
+	}
+}
---
+++ gateway/src/main/java/com/bjcareer/gateway/interceptor/LoggingInterceptor.java
@@ -0,0 +1,30 @@
+package com.bjcareer.gateway.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.bjcareer.gateway.common.Logger;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class LoggingInterceptor implements HandlerInterceptor {
+	private final Logger log;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
+		Exception {
+		log.setUrl(request.getRequestURI());
+		return true;
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+		Exception ex) throws Exception {
+		log.info("Response Status: {}", response.getStatus());
+	}
+}
+
