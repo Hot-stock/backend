a3020a3fbdd83554003114357fa7bb95bf62e66c
baejaewan <bjwan.career@gmail.com>
2024-10-31 15:12:44 +0900

    [refactor] Add MDC Filter
    #related to #66

---
+++ search/src/main/java/com/bjcareer/search/MdcTaskDecorator.java
@@ -0,0 +1,25 @@
+package com.bjcareer.search;
+
+import java.util.Map;
+
+import org.slf4j.MDC;
+import org.springframework.core.task.TaskDecorator;
+
+public class MdcTaskDecorator implements TaskDecorator {
+
+	@Override
+	public Runnable decorate(Runnable runnable) {
+		Map<String, String> contextMap = MDC.getCopyOfContextMap();
+		return () -> {
+			try {
+				// MDC 컨텍스트를 비동기 스레드에 설정
+				if (contextMap != null) {
+					MDC.setContextMap(contextMap);
+				}
+				runnable.run();
+			} finally {
+				MDC.clear();
+			}
+		};
+	}
+}
