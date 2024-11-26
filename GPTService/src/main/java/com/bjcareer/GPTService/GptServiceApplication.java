be6f4194f998dcdadd4506ea13bf5908f2f20469
baejaewan <bjwan.career@gmail.com>
2024-11-26 21:43:13 +0900

    hotfix

---
+++ GPTService/src/main/java/com/bjcareer/GPTService/GptServiceApplication.java
@@ -0,0 +1,17 @@
+package com.bjcareer.GPTService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
+public class GptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptServiceApplication.class, args);
+	}
+
+}
