446aa85614b9bb877ff885021293caeeba173215
baejaewan <bjwan.career@gmail.com>
2024-12-16 12:36:55 +0900

    [refactor] Remove didn't used file

--- search/src/main/java/com/bjcareer/search/application/port/in/KeywordUsecase.java
+++
@@ -1,9 +0,0 @@
-package com.bjcareer.search.application.port.in;
-
-import java.util.List;
-
-import com.bjcareer.search.domain.AbsoluteRankKeyword;
-
-public interface KeywordUsecase {
-	List<AbsoluteRankKeyword> getAbsoluteValueOfKeyword(String keyword);
-}
