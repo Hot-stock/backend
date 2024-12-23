be3182e7948cedb9d3b8c5cac094afd9ad249267
baejaewan <bjwan.career@gmail.com>
2024-12-23 13:55:59 +0900

    [refactor] 새로운 주식이 추가될 때 로고 이미지 갱신 코드 추가 related to #138

--- search/src/test/java/com/bjcareer/search/out/api/toss/TossServerAdapterTest.java
+++ search/src/test/java/com/bjcareer/search/out/api/toss/TossServerAdapterTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.bjcareer.search.out.api.toss.dtos.CandleResponseDTO;
+import com.bjcareer.search.out.api.toss.dtos.TossStockInfoDTO;
 
 @SpringBootTest
 class TossServerAdapterTest {
@@ -15,13 +16,17 @@ class TossServerAdapterTest {
 	private TossServerAdapter tossServerAdapter;
 
 	@Test
-	void test(){
+	void 토스_주식_ohlc차트_갱신_api(){
 		CandleResponseDTO responseDTO = tossServerAdapter.getStockPriceURI("041020", "day");
 		System.out.println("stockPriceURI = " + responseDTO);
 
 		System.out.println("sto = " + responseDTO.getResult());
+	}
 
-
+	@Test
+	void 토스_주식_정보(){
+		TossStockInfoDTO stockInfo = tossServerAdapter.getStockInfo("041020");
+		System.out.println("stockPriceURI = " + stockInfo.getResult().getLogoImageUrl());
 	}
 
 }
