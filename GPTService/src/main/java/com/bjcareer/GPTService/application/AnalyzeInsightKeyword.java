c5fa4f8447fb40d65834ab1c23e773668d0bd68a
baejaewan <bjwan.career@gmail.com>
2025-01-02 12:09:26 +0900

    [refactor] 테마에 관련된 뉴스 찾는 방식 변경 related to #152

--- GPTService/src/main/java/com/bjcareer/GPTService/application/AnalyzeInsightKeyword.java
+++ GPTService/src/main/java/com/bjcareer/GPTService/application/AnalyzeInsightKeyword.java
@@ -13,13 +13,11 @@ import org.springframework.stereotype.Service;
 import com.bjcareer.GPTService.domain.gpt.GPTNewsDomain;
 import com.bjcareer.GPTService.domain.gpt.GPTTriggerBackground;
 import com.bjcareer.GPTService.domain.gpt.insight.GPTInsight;
-import com.bjcareer.GPTService.domain.gpt.thema.GPTStockThema;
 import com.bjcareer.GPTService.domain.gpt.thema.GPTThema;
 import com.bjcareer.GPTService.out.api.gpt.insight.score.GPTInsightAdapter;
 import com.bjcareer.GPTService.out.persistence.document.GPTBackgroundRepository;
 import com.bjcareer.GPTService.out.persistence.document.GPTInsightRepository;
 import com.bjcareer.GPTService.out.persistence.document.GPTStockNewsRepository;
-import com.bjcareer.GPTService.out.persistence.document.GPTStockThemaNewsRepository;
 import com.bjcareer.GPTService.out.persistence.rdb.StockRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -31,7 +29,6 @@ import lombok.extern.slf4j.Slf4j;
 public class AnalyzeInsightKeyword {
 	private final GPTBackgroundRepository gptBackgroundRepository;
 	private final GPTInsightRepository gptInsightRepository;
-	private final GPTStockThemaNewsRepository gptStockThemaNewsRepository;
 	private final GPTStockNewsRepository gptStockNewsRepository;
 	private final GPTThemaNewsAnalyzeService gptThemaNewsAnalyzeService;
 	private final GPTInsightAdapter insightAdapter;
@@ -43,10 +40,13 @@ public class AnalyzeInsightKeyword {
 		List<GPTThema> gptThemas = new ArrayList<>();
 
 		List<GPTNewsDomain> raiseReason = gptStockNewsRepository.findByStockName(stockName);
-		List<String> links = raiseReason.stream().map(GPTNewsDomain::getLink).toList();
-		List<String> themaName = getThemaName(links);
+		List<String> themas = raiseReason.stream()
+			.map(t -> t.getThemaInfo().getName())
+			.collect(Collectors.toSet())
+			.stream()
+			.toList();
 
-		List<GPTTriggerBackground> backgrounds = themaName.stream()
+		List<GPTTriggerBackground> backgrounds = themas.stream()
 			.map(gptBackgroundRepository::findByThema)
 			.filter(Optional::isPresent)
 			.map(Optional::get)
@@ -76,11 +76,4 @@ public class AnalyzeInsightKeyword {
 
 		return insight;
 	}
-
-	private List<String> getThemaName(List<String> links) {
-		List<GPTStockThema> byLinkIn = gptStockThemaNewsRepository.findByLinkIn(links);
-		Set<String> collect = byLinkIn.stream().map(t -> t.getThemaInfo().getName()).collect(Collectors.toSet());
-		return new ArrayList<>(collect);
-	}
-
 }
